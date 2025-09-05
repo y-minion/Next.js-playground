5e58268c05ad1612b949294bb96d57b8058d73fc
y-minion <dudals2963@tukorea.ac.kr>
2025-09-05 17:53:23 +0900

    bookItem 컴포넌트 구현 완료

---
+++ section02/src/components/book-item.tsx
@@ -0,0 +1,27 @@
+import type { BookDate } from "@/types";
+import Link from "next/link";
+import style from "./book-item.module.css";
+
+export default function BookItem({
+  id,
+  title,
+  subTitle,
+  coverImgUrl,
+  description,
+  author,
+  publisher,
+}: BookDate) {
+  return (
+    <Link className={style.container} href={`/book/${id}`}>
+      <img className={style.img} src={coverImgUrl} />
+      <div>
+        <div className={style.title}>{title}</div>
+        <div className={style.subTitle}>{subTitle}</div>
+        <br />
+        <div className={style.author}>
+          {author} | {publisher}
+        </div>
+      </div>
+    </Link>
+  );
+}
---
+++ section02/src/types.ts
@@ -0,0 +1,9 @@
+export interface BookDate {
+  id: number;
+  title: string;
+  subTitle: string;
+  author: string;
+  publisher: string;
+  coverImgUrl: string;
+  description: string;
+}
