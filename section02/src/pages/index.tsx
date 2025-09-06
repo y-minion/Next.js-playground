362d830b31a084cfad958bd1b5190cc1b0028bae
y-minion <dudals2963@tukorea.ac.kr>
2025-09-06 19:11:53 +0900

    패치 로직 리팩토링
    promis.all 이용해 비동기 로직(데이터 패치 로직)들이 병렬적으로 모두 동시에 요청하도록 리팩토링 완료

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -7,8 +7,10 @@ import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-random-books";
 
 export const getServerSideProps = async () => {
-  const allBooks = await fetchBooks();
-  const randomBooks = await fetchRandomBooks();
+  const [allBooks, randomBooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks(),
+  ]);
 
   return {
     props: {
