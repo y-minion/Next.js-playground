2c969411662a846c642199a8ca2a1b0edcadc490
y-minion <dudals2963@tukorea.ac.kr>
2025-09-08 15:17:48 +0900

    SSG에서 fallback 옵션 설정 완료

--- section02/src/pages/book/[id].tsx
+++ section02/src/pages/book/[id].tsx
@@ -2,6 +2,7 @@
 import style from "./[Id].module.css";
 import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import fetchOneBook from "@/lib/fetch-one-books";
+import { useRouter } from "next/router";
 
 export const getStaticPaths = () => {
   return {
@@ -10,14 +11,26 @@ export const getStaticPaths = () => {
       { params: { id: "2" } },
       { params: { id: "3" } },
     ],
-    fallback: false,
+
+    fallback: true,
+    // false : Paths이외의 url파람 요청시 404 Notfound 화면 렌더링
+    // blocking : SSR 방식으로 추가 화면 렌더링
+    // true : SSR방식 + 데이터가 없는 폴백 상태의 페이지부터 반환 -> 이후 응답 오면 해당 페이지 렌더링
   };
 };
 
 export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params!.id;
   const book = await fetchOneBook(id as string);
-  console.log(book);
+
+  //응답 에러시, 즉 없는 페이지 요청 또는 패치 에러시 안내 페이지 렌더링 설정
+  if (!book) {
+    return {
+      //반환을 404 페이지로 설정
+      notFound: true,
+    };
+  }
+
   return {
     props: {
       book,
@@ -28,7 +41,10 @@ export const getStaticProps = async (context: GetStaticPropsContext) => {
 export default function Page({
   book,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
-  if (!book) return null;
+  const router = useRouter();
+  //useRouter의 isFallback옵션으로 로딩중 상태 알 수 있다.
+  if (router.isFallback) return "로딩중입니다.";
+  if (!book) return "문제가 발생했습니다. 다시 시도하세요!";
 
   const { title, subTitle, coverImgUrl, description, author, publisher } = book;
   return (
