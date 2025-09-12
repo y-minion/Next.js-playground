14f938b2d697da33818f24971bea74bafe356716
y-minion <dudals2963@tukorea.ac.kr>
2025-09-12 21:02:31 +0900

    section05 환경 구성 완료

---
+++ section05/src/app/(with-searchbar)/page.tsx
@@ -0,0 +1,55 @@
+import BookItem from "@/components/book-item";
+import style from "./page.module.css";
+import { BookData } from "@/types";
+
+const AllBooks = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`, {
+    cache: "no-store",
+  });
+  if (!res.ok) {
+    return <div>오류가 발생 했습니다...</div>;
+  }
+
+  const allBooks: BookData[] = await res.json();
+  console.log(allBooks); //오직 서버에서만 콘솔이 찍힌다. -> 기본적으로 넥스트는 모든 컴포넌트가 서버 컴포넌트임
+  return (
+    <div>
+      {allBooks.map((book) => {
+        return <BookItem key={book.id} {...book} />;
+      })}
+    </div>
+  );
+};
+
+const RecoBooks = async () => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+    { next: { revalidate: 3 } }
+  );
+  if (!res.ok) {
+    return <div>오류가 발생 했습니다...</div>;
+  }
+  const recoBooks: BookData[] = await res.json();
+  return (
+    <div>
+      {recoBooks.map((book) => {
+        return <BookItem key={book.id} {...book} />;
+      })}
+    </div>
+  );
+};
+
+export default function Home() {
+  return (
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        <RecoBooks />
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        <AllBooks />
+      </section>
+    </div>
+  );
+}
