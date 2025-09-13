cae3db1ce9e9a8f57e2b12c979854c8da92a2565
y-minion <dudals2963@tukorea.ac.kr>
2025-09-13 17:23:50 +0900

    없는 페이지에 접근할 경우 에러핸들링 로직 추가 완료

--- section05/src/app/book/[id]/page.tsx
+++ section05/src/app/book/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { notFound } from "next/navigation";
 import style from "./page.module.css";
 
 //미리 빌드 시점에 존재할 url파라미터를 넥스트 서버에 전달해준다. 이렇게 되면 빌드 시점에 미리 전달한 url파라미터에 대한 페이지들이 만들어 진다.
@@ -16,6 +17,10 @@ export default async function Page({
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${id}`
   );
   if (!res.ok) {
+    if (res.status === 404) {
+      //네비게이션의 notFound함수를 호출시 가장 가까운 not-Found 컴포넌트를 렌더링 해준다.
+      notFound();
+    }
     return <div>오류 발생....</div>;
   }
   const book = await res.json();
