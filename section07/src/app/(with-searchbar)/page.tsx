4a66d149b6f9b4ba01a8e5e0ded720b517782f5e
y-minion <dudals2963@tukorea.ac.kr>
2025-09-16 11:58:38 +0900

    섹션 07 환경구성완료

---
+++ section07/src/app/(with-searchbar)/page.tsx
@@ -0,0 +1,71 @@
+import BookItem from "@/components/book-item";
+import style from "./page.module.css";
+import { BookData } from "@/types";
+import delay from "util/delay";
+import { Suspense } from "react";
+import BookListSkeleton from "@/components/skeleton/book-list-skeleton";
+
+export const dynamic = "auto";
+//특정 페이지의 유형을 강제로 static, dynamic 페이지로 설정한다.
+// 1. auto
+// 2. force-dynamic
+// 3. force-static
+// 4. error
+
+const AllBooks = async () => {
+  await delay(1500);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`, {
+    cache: "force-cache",
+  });
+  if (!res.ok) {
+    return <div>오류가 발생 했습니다...</div>;
+  }
+
+  const allBooks: BookData[] = await res.json();
+  console.log(allBooks); //오직 서버에서만 콘솔이 찍힌다. -> 기본적으로 넥스트는 모든 컴포넌트가 서버 컴포넌트임
+  return (
+    <div>
+      {allBooks.map((book) => {
+        return <BookItem key={book.id} {...book} />;
+      })}
+    </div>
+  );
+};
+
+const RecoBooks = async () => {
+  await delay(3000);
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+    { next: { revalidate: 3 } }
+  );
+  if (!res.ok) {
+    return <div>오류가 발생 했습니다...</div>;
+  }
+  const recoBooks: BookData[] = await res.json();
+  return (
+    <div>
+      {recoBooks.map((book) => {
+        return <BookItem key={book.id} {...book} />;
+      })}
+    </div>
+  );
+};
+
+export default function Home() {
+  return (
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        <Suspense fallback={<BookListSkeleton count={3} />}>
+          <RecoBooks />
+        </Suspense>
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        <Suspense fallback={<BookListSkeleton count={3} />}>
+          <AllBooks />
+        </Suspense>
+      </section>
+    </div>
+  );
+}
---
+++ section07/src/app/(with-searchbar)/search/error.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { startTransition, useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+  return (
+    <div>
+      <h3>검색과정에서 에러가 발생했습니다.</h3>
+      <button
+        onClick={() => {
+          startTransition(() => {
+            router.refresh(); // 현재 페이지에 필요한 서버 컴포넌트들을 다시 불러온다
+            reset(); // 에러상태를 초기화한다, 컴포넌트들을 다시 렌더링 한다
+          });
+        }}
+      >
+        다시 시도
+      </button>
+    </div>
+  );
+}
