7886a5769a5d36f631f607dbc51b3989afe9ce57
y-minion <dudals2963@tukorea.ac.kr>
2025-09-16 16:30:23 +0900

    서버 액션 함수 분리 리팩토링

---
+++ section07/src/actions/create-review.action.ts
@@ -0,0 +1,28 @@
+"use server"; //서버에서만 실행이 되도록 만들어주는 지시자를 사용한다.
+
+export default async function createReviewAction(formData: FormData) {
+  console.log("server action called");
+
+  //이때 formData의 타입으로 string이나 파일타입이 올 수 있다.
+  //하지만 우리는 현재 string만 사용할 것이기 때문에 값이 존재하면 문자열로 변환한다.
+  const bookId = formData.get("bookId")?.toString();
+  const content = formData.get("content")?.toString();
+  const author = formData.get("author")?.toString();
+
+  //서버측에서 클라이언트에 비어있는 항목이 있으면 올바르지 않은 요청으로 판단하여 종료한다
+  if (!bookId || !content || !author) return;
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
+      {
+        method: "POST",
+        body: JSON.stringify({ bookId, content, author }),
+      }
+    );
+
+    console.log(response);
+  } catch (err) {
+    console.error(err);
+    return;
+  }
+}
--- section07/src/app/book/[id]/page.tsx
+++ section07/src/app/book/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from "next/navigation";
 import style from "./page.module.css";
+import createReviewAction from "@/actions/create-review.action";
 
 //미리 빌드 시점에 존재할 url파라미터를 넥스트 서버에 전달해준다. 이렇게 되면 빌드 시점에 미리 전달한 url파라미터에 대한 페이지들이 만들어 진다.
 export function generateStaticParams() {
@@ -40,38 +41,12 @@ async function BookDetail({ bookId }: { bookId: string }) {
 }
 
 function ReviewEditor({ bookId }: { bookId: string }) {
-  async function createReviewAction(formData: FormData) {
-    "use server"; //서버에서만 실행이 되도록 만들어주는 지시자를 사용한다.
-    console.log("server action called");
-
-    //이때 formData의 타입으로 string이나 파일타입이 올 수 있다.
-    //하지만 우리는 현재 string만 사용할 것이기 때문에 값이 존재하면 문자열로 변환한다.
-    const content = formData.get("content")?.toString();
-    const author = formData.get("author")?.toString();
-
-    //서버측에서 클라이언트에 비어있는 항목이 있으면 올바르지 않은 요청으로 판단하여 종료한다
-    if (!content || !author) return;
-    try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
-        {
-          method: "POST",
-          body: JSON.stringify({ bookId, content, author }),
-        }
-      );
-
-      console.log(response);
-    } catch (err) {
-      console.error(err);
-      return;
-    }
-  }
-
   return (
     <section>
       {/* 폼이 제출될때 서버에서 실행되는 함수를 action을 통해 전달한다 */}
       <form action={createReviewAction}>
         {/* Input의 모든 항목을 입력해야 제출 할 수 있도록 한다 */}
+        <input name="bookId" value={bookId} hidden readOnly />
         <input required name="content" placeholder="리뷰 내용" />
         <input required name="author" placeholder="작성자" />
         <button type="submit">작성하기</button>
