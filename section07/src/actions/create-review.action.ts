4574bc854051bef3e7a4955c00b805747ee2c5dd
y-minion <dudals2963@tukorea.ac.kr>
2025-09-17 12:38:31 +0900

    클라이언트 컴포넌트에서 서버액션에 따라 반응 할 수 있도록 useActionState를 사용해 구현 완료

--- section07/src/actions/create-review.action.ts
+++ section07/src/actions/create-review.action.ts
@@ -1,9 +1,10 @@
 "use server";
-import { revalidatePath, revalidateTag } from "next/cache";
+import { revalidateTag } from "next/cache";
+import delay from "util/delay";
 
 //서버에서만 실행이 되도록 만들어주는 지시자를 사용한다.
-
-export default async function createReviewAction(formData: FormData) {
+//useActionState 훅을 사용하면 해당 훅이 반환하는 상태 값(첫번째 변수)이 첫번째 매개변수로 전달이 된다. 하지만 현재 서버액션 함수에서는 사용하지 않으므로 언더바 처리한다.
+export default async function createReviewAction(_: any, formData: FormData) {
   console.log("server action called");
 
   //이때 formData의 타입으로 string이나 파일타입이 올 수 있다.
@@ -13,20 +14,27 @@ export default async function createReviewAction(formData: FormData) {
   const author = formData.get("author")?.toString();
 
   //서버측에서 클라이언트에 비어있는 항목이 있으면 올바르지 않은 요청으로 판단하여 종료한다
-  if (!bookId || !content || !author) return;
+  if (!bookId || !content || !author)
+    return { status: false, error: "리뷰내용과 작성자를 입력해 주세요" };
   try {
+    await delay(2000);
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
+      `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review12`,
       {
         method: "POST",
         body: JSON.stringify({ bookId, content, author }),
       }
     );
+
+    if (!response.ok) throw new Error(response.statusText);
     //패치가 성공적으로 이루어 지면 해당 페이지를 새롭게 만들어서 클라이언트에 보내준다
     revalidateTag(`review-${bookId}`);
-    console.log(response);
+    return {
+      status: true,
+      error: "",
+    };
   } catch (err) {
     console.error(err);
-    return;
+    return { status: false, error: `리뷰 저장에 실패했습니다:${err}` };
   }
 }
--- section07/src/components/review-editor.tsx
+++ section07/src/components/review-editor.tsx
@@ -1,17 +1,42 @@
+"use client";
+
+import { useActionState, useEffect } from "react";
 import style from "./review-editor.module.css";
 import createReviewAction from "@/actions/create-review.action";
 
 export default function ReviewEditor({ bookId }: { bookId: string }) {
+  const [state, formAction, isPending] = useActionState(
+    createReviewAction,
+    null
+  );
+
+  //useActionState가 반환하는 상태를 통해 서버 액션 함수에서 에러가 발생하면 에러 메세지를 띄울 수 있도록 한다.
+  useEffect(() => {
+    if (state && !state.status) alert(state.error);
+  }, [state]);
+
   return (
     <section>
       {/* 폼이 제출될때 서버에서 실행되는 함수를 action을 통해 전달한다 */}
-      <form className={style.form_container} action={createReviewAction}>
+      <form className={style.form_container} action={formAction}>
         {/* Input의 모든 항목을 입력해야 제출 할 수 있도록 한다 */}
         <input name="bookId" value={bookId} hidden readOnly />
-        <textarea required name="content" placeholder="리뷰 내용" />
+        <textarea
+          required
+          disabled={isPending}
+          name="content"
+          placeholder="리뷰 내용"
+        />
         <div className={style.submit_container}>
-          <input required name="author" placeholder="작성자" />
-          <button type="submit">작성하기</button>
+          <input
+            required
+            disabled={isPending}
+            name="author"
+            placeholder="작성자"
+          />
+          <button disabled={isPending} type="submit">
+            {isPending ? "..." : "작성하기"}
+          </button>
         </div>
       </form>
     </section>
