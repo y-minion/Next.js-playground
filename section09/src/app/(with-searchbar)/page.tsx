b757ecffe7d0f03475c67972ee30186f3e0ddd64
y-minion <dudals2963@tukorea.ac.kr>
2025-09-21 22:00:02 +0900

    인덱스 페이지 SEO 설정 완료

--- section09/src/app/(with-searchbar)/page.tsx
+++ section09/src/app/(with-searchbar)/page.tsx
@@ -4,6 +4,7 @@ import { BookData } from "@/types";
 import delay from "util/delay";
 import { Suspense } from "react";
 import BookListSkeleton from "@/components/skeleton/book-list-skeleton";
+import { Metadata } from "next";
 
 export const dynamic = "auto";
 //특정 페이지의 유형을 강제로 static, dynamic 페이지로 설정한다.
@@ -11,6 +12,16 @@ export const dynamic = "auto";
 // 2. force-dynamic
 // 3. force-static
 // 4. error
+
+export const metadata: Metadata = {
+  title: "한입 북스",
+  description: "한입 북스에 등록된 도서를 만나보세요",
+  openGraph: {
+    title: "한입 북스",
+    description: "한입 북스에 등록된 도서를 만나보세요",
+    images: ["/thumbnail.png"],
+  },
+};
 
 const AllBooks = async () => {
   await delay(1500);
